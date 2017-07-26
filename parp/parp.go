9cc6dd29b87556e18e92fb1ef461582e01fc9646
lilang <lilang@pheicloud.com>
2017-07-26 09:22:44 +0800

    parp add interactive feature

--- parp/parp.go
+++ parp/parp.go
@@ -5,62 +5,70 @@ import (
     "github.com/google/gopacket"
     "github.com/google/gopacket/pcap"
 
-    // "fmt"
+    "fmt"
     "bytes"
     // "encoding/binary"
 )
 
 var (
     handle *pcap.Handle
+    pchan chan gopacket.Packet
+    input int
 )
 
 func ExposeSelf(packet gopacket.Packet) {
 
     origin := packet.Data()
 
-    // fmt.Println(packet)
+    var buff bytes.Buffer
+    buff.Write(origin[6:12])
+    buff.Write(origin[0:6])
+    buff.Write([]byte{0x08, 0x06})
 
-    if (origin[13] == 0x06 && origin[4] == 0xd6 && origin[5] === 0x29) {
+    // hardware type
+    buff.Write([]byte{0x00, 0x01})
+    // protocol type IPv4
+    buff.Write([]byte{0x08, 0x00})
+    // hardware size & protocol size
+    buff.Write([]byte{0x06, 0x04})
+    // operation code 00,01 request; 00, 02 reply
+    buff.Write([]byte{0x00, 0x02})
+    // sender Mac address
+    buff.Write([]byte{0x12, 0x34, 0x56, 0x78, 0x90, 0x12})
+    // sender Ip address
+    buff.Write(origin[38:42])
 
-        // fmt.Printf("arp request from %v\r\n", origin[28:32])
+    buff.Write(origin[22:32])
 
-        var buff bytes.Buffer
-        buff.Write(origin[6:12])
-        buff.Write(origin[0:6])
-        buff.Write([]byte{0x08, 0x06})
+    out := buff.Bytes()
 
-        // hardware type
-        buff.Write([]byte{0x00, 0x01})
-        // protocol type IPv4
-        buff.Write([]byte{0x08, 0x00})
-        // hardware size & protocol size
-        buff.Write([]byte{0x06, 0x04})
-        // operation code 00,01 request; 00, 02 reply
-        buff.Write([]byte{0x00, 0x02})
-        // sender Mac address
-        buff.Write([]byte{0x12, 0x34, 0x56, 0x78, 0x90, 0x12})
-        // sender Ip address
-        buff.Write(origin[38:42])
-
-        buff.Write(origin[22:32])
-
-        out := buff.Bytes()
-
-        handle.WritePacketData(out)
-
-    }
+    handle.WritePacketData(out)
 }
 
 func main () {
 
+    interfaces, _ := pcap.FindAllDevs()
+
+    for i := 0; i < len(interfaces); i++ {
+        fmt.Printf("[%d] %v\n", i, interfaces[i].Name)
+
+        var int_addrs = interfaces[i].Addresses
+        for j := 0; j < len(int_addrs); j++ {
+            fmt.Printf("    %v\n",int_addrs[j].IP)
+        }
+
+    }
+    fmt.Printf("which interface you will use to cap[0~%v]:", len(interfaces) - 1);
+    fmt.Scanf("%d", &input)
+
+    pchan = make(chan gopacket.Packet)
+
     handle, _ = pcap.OpenLive(
-        "en0",
+        interfaces[input].Name,
         1600,
         true,
         pcap.BlockForever,
     )
-
-    // handle.SetBPFFilter("tcp and port 80")
 
     packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 
